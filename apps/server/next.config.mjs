40cf5ba95cd2c0d0296f3eeda3a344a7972455f0
Sagar <sagar@aryal.me>
2023-03-31 13:39:52 +0200

    Update next.config.mjs

--- apps/server/next.config.mjs
+++ apps/server/next.config.mjs
@@ -8,11 +8,18 @@
 const config = {
   reactStrictMode: true,
 
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
