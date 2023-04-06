2c12f763b006d9f145c285069e5dd3f89e0e923b
Mayank <mayankkumawat015@gmail.com>
2023-04-06 18:53:50 +0530

    fixes build

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import App from './apps/nativeapp/app/App.tsx';
-import { name as appName } from './apps/nativeapp/app.json';
-
-AppRegistry.registerComponent(appName, () => App);
