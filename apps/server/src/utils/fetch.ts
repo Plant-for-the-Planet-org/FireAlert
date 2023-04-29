7416c0155418b0797710b7db1688644d99b8e641
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-04-29 12:34:58 +0545

    Fixed fetch code

--- apps/server/src/utils/fetch.ts
+++ apps/server/src/utils/fetch.ts
@@ -1,18 +1,22 @@
-export const checkIfUserIsPlanetRO = async (bearer_token: string) => {
-    // send a request to https://app.plant-for-the-planet.org/app/profile with authorization headers of Bearer token using bearer_token
-    // Check the response the the above request for type = "tpo"
-    // If yes return true, else return false
-    const response = await fetch(
-        "https://app-staging.plant-for-the-planet.org/app/profile",
-        {
-            headers: {
-                Authorization: bearer_token,
-            },
-        }
-    );
-    const data = await response.json();
-    return data.type === "tpo";
-}
+export const checkIfUserIsPlanetRO = async (
+    bearer_token: string
+): Promise<boolean> => {
+    try {
+        const response = await fetch(
+            "https://app-staging.plant-for-the-planet.org/app/profile",
+            {
+                headers: {
+                    Authorization: bearer_token,
+                },
+            }
+        );
+        const data = await response.json();
+        return data.type === "tpo";
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
 
 export const fetchProjectsWithSitesForUser = async (bearer_token: string) => {
     // fetch data from https://app.plant-for-the-planet.org/app/profile/projects?_scope=sites with authorization headers of Bearer token using bearer_token
@@ -36,21 +40,45 @@ export const fetchAllProjectsWithSites = async () => {
     return data;
 }
 
-type ProfileData = {
-    name: string;
-};
 
-export const getNameFromPPApi = async (
-    bearer_token: string
-): Promise<ProfileData> => {
-    const response = await fetch(
-        "https://app.plant-for-the-planet.org/app/profile",
-        {
+export const getNameFromPPApi = async (bearer_token: string): Promise<string> => {
+    const initialUrl = "https://app.plant-for-the-planet.org/app/profile";
+    try {
+        let response = await fetch(initialUrl, {
             headers: {
                 Authorization: bearer_token,
             },
+        });
+
+        if (response.status === 303) {
+            const redirectUrl = response.headers.get("Location")!;
+            response = await fetch(redirectUrl, {
+                headers: {
+                    Authorization: bearer_token,
+                },
+            });
         }
-    );
-    const data = await response.json();
-    return { name: data.name };
+        if (response.ok) {
+            const data = await response.json();
+            if(data.name){
+                return data.name;
+            }else if(data.firstname && data.lastname){
+                const name = data.firstname + " " + data.lastname
+                return name
+            }else if(data.firstname || data.lastname){
+                const firstname = data.firstname || ""
+                const lastname = data.lastname || ""
+                const name = firstname + " " + lastname
+                return name
+            }
+        } else {
+            console.error(`Error fetching profile data: Error: HTTP error! status: ${response.status}`);
+            return "";
+        }
+    } catch (error) {
+        console.error(`Error fetching profile data: ${error}`);
+        return "";
+    }
 };
+
+
