69418efbd807924c7f8014cd97908fdc05004551
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-05-02 22:11:02 +0545

    Made radius optional

--- apps/server/src/server/api/zodSchemas/site.schema.ts
+++ apps/server/src/server/api/zodSchemas/site.schema.ts
@@ -20,7 +20,7 @@ export const createSiteSchema = z.object({
     type: z.enum(["Point", "Polygon", "MultiPolygon"]),
     name: z.string().optional(),
     geometry: z.union([PointSchema, PolygonSchema, MultiPolygonSchema]),
-    radius: z.enum(["inside", "within5km", "within10km", "within100km"]),
+    radius: z.enum(["inside", "within5km", "within10km", "within100km"]).optional(),
     isMonitored: z.boolean().optional(),
     userId: z.string().optional(),
     projectId: z.string().nullable().optional(),
