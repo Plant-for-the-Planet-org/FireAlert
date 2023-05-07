54b878a503fb9fd8c07a323f0a67f2ccc7f4e4db
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-05-08 02:26:40 +0545

    Revert last commit

--- apps/server/src/env.mjs
+++ apps/server/src/env.mjs
@@ -49,7 +49,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_PRISMA_URL ? process.env.DATABASE_PRISMA_URL:process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
@@ -65,6 +65,7 @@ const processEnv = {
   TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
   SMTP_URL: process.env.SMTP_URL,
   EMAIL_FROM: process.env.EMAIL_FROM,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
