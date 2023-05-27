205fce8a082eeabbf9948d3abc1a9843d2929958
Sagar <sagar@aryal.me>
2023-05-27 12:16:02 +0200

    Add Sentry Profiling

--- apps/server/sentry.server.config.ts
+++ apps/server/sentry.server.config.ts
@@ -4,6 +4,7 @@
 
 import * as Sentry from "@sentry/nextjs";
 import { env } from "./src/env.mjs";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
@@ -13,4 +14,10 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  
+  profilesSampleRate: 1,
+  integrations: [
+    // Add profiling integration to list of integrations
+    new ProfilingIntegration(),
+  ],
 });
