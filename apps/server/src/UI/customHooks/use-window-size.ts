838bcd7cb8e74d38e7ca16ac5135eff5aa5497e8
Shreyaschorge <shreyaschorge81@gmail.com>
2023-06-09 17:28:02 +0530

    Add useWindowSize hook

---
+++ apps/server/src/UI/customHooks/use-window-size.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+const useWindowSize = (): WindowSize => {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
