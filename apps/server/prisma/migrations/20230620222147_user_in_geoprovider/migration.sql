8b7da406bf0bc858242b8058b83e10bc3b3caa8b
Sagar <sagar@aryal.me>
2023-06-21 02:30:04 +0200

    add plan concept to user table
    eg: basic | member | developer

--- apps/server/prisma/migrations/20230620222147_user_in_geoprovider/migration.sql
+++ apps/server/prisma/migrations/20230620222147_user_in_geoprovider/migration.sql
@@ -2,5 +2,8 @@
 ALTER TABLE "GeoEventProvider"
 ADD COLUMN "userId" TEXT;
 
+ALTER TABLE "User"
+ADD COLUMN "plan" TEXT NOT NULL DEFAULT 'basic';
+
 -- AddForeignKey
 ALTER TABLE "GeoEventProvider" ADD CONSTRAINT "GeoEventProvider_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
