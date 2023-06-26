e5e867c3f1de0e79bafed2b51581756858b5ab84
Mayank <mayankkumawat015@gmail.com>
2023-06-27 00:34:27 +0530

    fixed node version upgrade build error | added createAsyncStoragePersister

--- apps/nativeapp/app/services/trpc.tsx
+++ apps/nativeapp/app/services/trpc.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Config from 'react-native-config';
 import {createTRPCReact, httpBatchLink} from '@trpc/react-query';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client';
+import {createAsyncStoragePersister} from '@tanstack/query-async-storage-persister';
 
 import {store} from '../redux/store';
 import type {AppRouter} from '../../../server/src/server/api/root';
 
 export const trpc = createTRPCReact<AppRouter>();
+
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+});
 
 export const TRPCProvider: React.FC<{children: React.ReactNode}> = ({
   children,
@@ -31,8 +38,14 @@ export const TRPCProvider: React.FC<{children: React.ReactNode}> = ({
   );
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{persister: asyncStoragePersister}}>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </trpc.Provider>
+    </PersistQueryClientProvider>
   );
 };
