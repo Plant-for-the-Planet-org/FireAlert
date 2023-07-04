183de8e39ebdbcc5dcb59ae75c2baa2b996398dc
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-07-04 22:20:17 +0545

    Test integration of pulsing dot

--- apps/server/src/Components/AlertId/MapComponent.tsx
+++ apps/server/src/Components/AlertId/MapComponent.tsx
@@ -32,11 +32,118 @@ const MapComponent: FC<Props> = ({ alertData }) => {
     const onMapLoad = React.useCallback(() => {
         const map = mapRef?.current?.getMap();
         map?.setStyle(mapStyle);
+
+        const size = 200;
+
+        // This implements `StyleImageInterface`
+        // to draw a pulsing dot icon on the map.
+        const pulsingDot = {
+            width: size,
+            height: size,
+            data: new Uint8Array(size * size * 4),
+
+            // When the layer is added to the map,
+            // get the rendering context for the map canvas.
+            onAdd: function () {
+                const canvas = document.createElement('canvas');
+                canvas.width = this.width;
+                canvas.height = this.height;
+                this.context = canvas.getContext('2d');
+            },
+
+            // Call once before every frame where the icon will be used.
+            render: function () {
+                const duration = 1000;
+                const t = (performance.now() % duration) / duration;
+
+                const radius = (size / 2) * 0.3;
+                const outerRadius = (size / 2) * 0.7 * t + radius;
+                const context = this.context;
+
+                // Draw the outer circle.
+                context.clearRect(0, 0, this.width, this.height);
+                context.beginPath();
+                context.arc(
+                    this.width / 2,
+                    this.height / 2,
+                    outerRadius,
+                    0,
+                    Math.PI * 2
+                );
+                context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+                context.fill();
+
+                // Draw the inner circle.
+                context.beginPath();
+                context.arc(
+                    this.width / 2,
+                    this.height / 2,
+                    radius,
+                    0,
+                    Math.PI * 2
+                );
+                context.fillStyle = 'rgba(255, 100, 100, 1)';
+                context.strokeStyle = 'white';
+                context.lineWidth = 2 + 4 * (1 - t);
+                context.fill();
+                context.stroke();
+
+                // Update this image's data with data from the canvas.
+                this.data = context.getImageData(
+                    0,
+                    0,
+                    this.width,
+                    this.height
+                ).data;
+
+                // Continuously repaint the map, resulting
+                // in the smooth animation of the dot.
+                map.triggerRepaint();
+
+                // Return `true` to let the map know that the image was updated.
+                return true;
+            }
+        };
+        map?.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+        // Ensure map and map style is loaded
+        if (map) {
+            map.on('styledata', () => {
+                // Check if the source and layer already exists, only add if they don't.
+                if (!map.getSource('dot-point')) {
+                    map.addSource('dot-point', {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                {
+                                    'type': 'Feature',
+                                    'geometry': {
+                                        'type': 'Point',
+                                        'coordinates': [longitude, latitude] // icon position [lng, lat]
+                                    }
+                                }
+                            ]
+                        }
+                    });
+
+                    map.addLayer({
+                        'id': 'layer-with-pulsing-dot',
+                        'type': 'symbol',
+                        'source': 'dot-point',
+                        'layout': {
+                            'icon-image': 'pulsing-dot'
+                        }
+                    });
+                }
+            });
+        }
     }, [mapStyle]);
+
+
 
     return (
         <Map
-            // mapLib={import('maplibre-gl')}
             initialViewState={viewState}
             onLoad={onMapLoad}
             onMove={evt => setViewState(evt.viewState)}
@@ -45,13 +152,6 @@ const MapComponent: FC<Props> = ({ alertData }) => {
             mapStyle={mapStyle}
             scrollZoom={false}
         >
-            <Marker longitude={longitude} latitude={latitude} anchor="bottom">
-                <div className={classes.vectorAnimationContainer}>
-                    <Image src={ellipse1} alt="Ellipse 1" className={classes.pulseAnimation1} />
-                    <Image src={vector} alt="Map Focus" className={classes.vector} />
-                    <Image src={ellipse2} alt="Ellipse 2" className={classes.pulseAnimation2} style={{ animationDelay: '0.5s' }} />
-                </div>
-            </Marker>
             <NavigationControl />
             <ScaleControl />
             <FullscreenControl />
