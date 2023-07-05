b203742237bc34aed330f42bb28fc4188b8705c9
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-07-05 19:58:42 +0545

    Finalize animation in alert page

--- apps/server/src/Components/AlertId/MapComponent.tsx
+++ apps/server/src/Components/AlertId/MapComponent.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import Map, { NavigationControl, ScaleControl, FullscreenControl, MapRef, Marker } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import mapStyle from '../../data/mapStyleOutput.json'
-import vector from '../../../public/alertPage/mapFocus/Vector.png'
+import Image from 'next/image';
+import vector from '../../../public/alertPage/Vector.png'
+import { highlightWave } from '../../../../nativeapp/app/assets/animation/lottie'
+import Lottie from 'react-lottie';
+import classes from './MapComponent.module.css'
 
 interface AlertData {
     latitude: string;
@@ -28,55 +32,13 @@ const MapComponent: FC<Props> = ({ alertData }) => {
     const onMapLoad = React.useCallback(async () => {
         const map = mapRef?.current?.getMap();
         map?.setStyle(mapStyle);
-
-        const vectorImg = new Image();
-        vectorImg.src = vector.src;
-
-        await new Promise((resolve, reject) => {
-            vectorImg.onload = resolve;
-            vectorImg.onerror = reject;
-        });
-
-        // Ensure map and map style is loaded
-        if (map) {
-            map.on('styledata', () => {
-                // Check if the image has completed loading and if the image is not already added
-                if (vectorImg.complete && !map.hasImage('vector-img')) {
-                    map.addImage('vector-img', vectorImg, { pixelRatio: 6 });
-                }
-
-                // Check if the source and layer already exists, only add if they don't.
-                if (!map.getSource('dot-point')) {
-                    map.addSource('dot-point', {
-                        'type': 'geojson',
-                        'data': {
-                            'type': 'FeatureCollection',
-                            'features': [
-                                {
-                                    'type': 'Feature',
-                                    'geometry': {
-                                        'type': 'Point',
-                                        'coordinates': [longitude, latitude] // icon position [lng, lat]
-                                    }
-                                }
-                            ]
-                        }
-                    });
-
-                    map.addLayer({
-                        'id': 'layer-with-pulsing-dot',
-                        'type': 'symbol',
-                        'source': 'dot-point',
-                        'layout': {
-                            'icon-image': 'vector-img'
-                        }
-                    });
-                }
-            });
-        }
     }, [mapStyle]);
 
-
+    const defaultLottieOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: highlightWave,
+    };
 
     return (
         <Map
@@ -88,6 +50,16 @@ const MapComponent: FC<Props> = ({ alertData }) => {
             mapStyle={mapStyle}
             scrollZoom={false}
         >
+            <Marker longitude={longitude} latitude={latitude} anchor="bottom">
+                <div className={classes.vectorAnimationContainer}>
+                    <Image src={vector} alt="Map Focus" className={classes.vector} />
+                    <div className={classes.lottieAnimation}>
+                        <Lottie
+                            options={defaultLottieOptions}
+                        />
+                    </div>
+                </div>
+            </Marker>
             <NavigationControl />
             <ScaleControl />
             <FullscreenControl />
