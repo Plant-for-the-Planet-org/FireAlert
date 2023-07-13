3d91bb5d4cd1e163418cfc21f9c579993a2eb773
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2023-07-13 21:25:00 +0545

    Protect routes from XSS
    Handle zodError server-side
    Sanitize strings
    Give min-max length to string
    Restrict id input to cuid
    Add validator package

--- apps/server/src/server/api/zodSchemas/alertMethod.schema.ts
+++ apps/server/src/server/api/zodSchemas/alertMethod.schema.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 import phone from 'phone'
+import validator from 'validator';
 
 export const createAlertMethodSchema = z.object({
     method: z.enum(["email", "sms", "device", "whatsapp", "webhook"]),
     destination: z.string({
         required_error: 'Destination of alert method must be specified'
-    }),
+    }).refine((value) => {
+            const sanitized = validator.escape(value);
+            return sanitized === value;
+        }, {
+            message: 'Contains invalid characters',
+        }),
     deviceName: z.string().optional(),
     deviceId: z.string().optional(),
 }).refine((obj) => {
@@ -14,27 +20,35 @@ export const createAlertMethodSchema = z.object({
         const { isValid } = phone(obj.destination)
         return isValid;
     }
+    if (obj.method === 'email') {
+        return z.string().email().safeParse(obj.destination).success;
+    }
     return true; // Return true for other methods
 }, {
     message: 'Must be a valid phone number in E.164 format when the method is "sms"'
-})
+});
 
 export const params = z.object({
-    alertMethodId: z.string(),
-})
+    alertMethodId: z.string().cuid({ message: "Invalid CUID" }),
+});
 
 export const verifySchema = z.object({
     params,
     body: z.object({
-        token: z.string()
+        token: z.string().length(5, { message: "Invalid OTP" }).refine(value => {
+            const sanitized = validator.escape(value);
+            return sanitized === value;
+        }, {
+            message: 'OTP Contains invalid characters',
+        })
     })
-})
+});
 
 export const updateAlertMethodSchema = z.object({
     params,
     body: z.object({
         isEnabled: z.boolean(),
     })
-})
+});
 
 export type ParamsType = z.infer<typeof params>;
