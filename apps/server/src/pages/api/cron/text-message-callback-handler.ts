b6031d719eb9d812b939d637acc74da24ea356a2
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2024-02-09 16:59:08 +0545

    Update text-message-callback-handler.ts

--- apps/server/src/pages/api/cron/text-message-callback-handler.ts
+++ apps/server/src/pages/api/cron/text-message-callback-handler.ts
@@ -8,7 +8,6 @@ import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
-        res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
         return;
     }
