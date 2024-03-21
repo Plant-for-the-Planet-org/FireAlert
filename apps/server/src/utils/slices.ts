ee8ee0d6b9ae2a4f3896d197f509bbb5bc99c90e
Aashish Dhakal <85501584+dhakalaashish@users.noreply.github.com>
2024-03-21 18:10:13 +0545

    Divide slices for equal landmass

--- apps/server/src/utils/slices.ts
+++ apps/server/src/utils/slices.ts
@@ -3,237 +3,203 @@ const slices = {
     name: 'Slice 10',
     bbox: '-180,-90,180,-30',
     coordinates: [
-      [
-        [-180, -90],
-        [180, -90],
-        [180, -30],
-        [-180, -30],
-        [-180, -90],
-      ],
+      [-180, -90],
+      [180, -90],
+      [180, -30],
+      [-180, -30],
+      [-180, -90]
     ],
     description: 'Covers the Southernmost regions of the world including the majority of Antarctica and parts of Southern South America.',
   },
   '21': {
     name: 'Slice 21',
-    bbox: '-180,-30,-30,-15',
+    bbox: '-180,-30,-60,-15',
     coordinates: [
-      [
-        [-180, -30],
-        [-30, -30],
-        [-30, -15],
-        [-180, -15],
-        [-180, -30],
-      ],
+      [-180, -30],
+      [-60, -30],
+      [-60, -15],
+      [-180, -15],
+      [-180, -30]
     ],
-    description: 'Covers Mid-lower South America, containing upper Chile, Paraguay, lower Brazil, upper Argentina, lower Bolivia.',
+    description: 'Covers left part of mid-lower South America.',
   },
   '22': {
     name: 'Slice 22',
-    bbox: '-30,-30,60,-15',
+    bbox: '-60,-30,30,-15',
     coordinates: [
-      [
-        [-30, -30],
-        [60, -30],
-        [60, -15],
-        [-30, -15],
-        [-30, -30],
-      ],
+      [-60, -30],
+      [30, -30],
+      [30, -15],
+      [-60, -15],
+      [-60, -30]
     ],
-    description: 'Covers Lower Africa, including upper South Africa, Namibia, Botswana, Zimbabwe, parts of Madagascar.',
+    description: 'Covers right part of mid-lower South America, and left part of Lower Africa.',
   },
   '23': {
     name: 'Slice 23',
-    bbox: '60,-30,180,-15',
+    bbox: '30,-30,180,-15',
     coordinates: [
-      [
-        [60, -30],
-        [180, -30],
-        [180, -15],
-        [60, -15],
-        [60, -30],
-      ],
+      [30, -30],
+      [180, -30],
+      [180, -15],
+      [30, -15],
+      [30, -30]
     ],
-    description: 'Covers the Upper part of Australia.',
+    description: 'Covers right part of Lower Africa, and upper part of Australia.',
   },
   '31': {
     name: 'Slice 31',
-    bbox: '-180,-15,-30,0',
+    bbox: '-180,-15,-60,0',
     coordinates: [
-      [
-        [-180, -15],
-        [-30, -15],
-        [-30, 0],
-        [-180, 0],
-        [-180, -15],
-      ],
+      [-180, -15],
+      [-60, -15],
+      [-60, 0],
+      [-180, 0],
+      [-180, -15]
     ],
-    description: 'Covers Mid-upper South America, covering upper Brazil, Peru, upper Bolivia.',
+    description: 'Covers left part of Mid-upper South America.',
   },
   '32': {
     name: 'Slice 32',
-    bbox: '-30,-15,60,0',
+    bbox: '-60,-15,30,0',
     coordinates: [
-      [
-        [-30, -15],
-        [60, -15],
-        [60, 0],
-        [-30, 0],
-        [-30, -15],
-      ],
+      [-60, -15],
+      [30, -15],
+      [30, 0],
+      [-60, 0],
+      [-60, -15]
     ],
-    description: 'Covers Lower-mid Africa, with parts of Angola, Zambia, Malawi, Tanzania, Kenya, Democratic Republic of the Congo, Gabon, Republic of the Congo.',
+    description: 'Covers right part of Mid-upper South America, and left part of lower-mid Africa.',
   },
   '33': {
     name: 'Slice 33',
-    bbox: '60,-15,180,0',
+    bbox: '30,-15,180,0',
     coordinates: [
-      [
-        [60, -15],
-        [180, -15],
-        [180, 0],
-        [60, 0],
-        [60, -15],
-      ],
+      [30, -15],
+      [180, -15],
+      [180, 0],
+      [30, 0],
+      [30, -15]
     ],
-    description: 'Covers Papua New Guinea and parts of Indonesia.',
+    description: 'Covers right part of lower-mid Africa, and Papua New Guinea and parts of Indonesia.',
   },
   '41': {
     name: 'Slice 41',
-    bbox: '-180,0,-30,15',
+    bbox: '-180,0,-60,15',
     coordinates: [
-      [
-        [-180, 0],
-        [-30, 0],
-        [-30, 15],
-        [-180, 15],
-        [-180, 0],
-      ],
+      [-180, 0],
+      [-60, 0],
+      [-60, 15],
+      [-180, 15],
+      [-180, 0]
     ],
-    description: 'Covers Upper South America, containing upper Colombia, Venezuela, Guyana, Suriname, Panama, Costa Rica, Nicaragua.',
+    description: 'Covers left part of Upper South America.',
   },
   '42': {
     name: 'Slice 42',
-    bbox: '-30,0,60,15',
+    bbox: '-60,0,30,15',
     coordinates: [
-      [
-        [-30, 0],
-        [60, 0],
-        [60, 15],
-        [-30, 15],
-        [-30, 0],
-      ],
+      [-60, 0],
+      [30, 0],
+      [30, 15],
+      [-60, 15],
+      [-60, 0]
     ],
-    description: 'Covers Upper-mid Africa, covering Nigeria, Cameroon, parts of Sudan, Ethiopia, Somalia.',
+    description: 'Covers right part of Upper South America and left part of Upper-mid Africa.',
   },
   '43': {
     name: 'Slice 43',
-    bbox: '60,0,180,15',
+    bbox: '30,0,180,15',
     coordinates: [
-      [
-        [60, 0],
-        [180, 0],
-        [180, 15],
-        [60, 15],
-        [60, 0],
-      ],
+      [30, 0],
+      [180, 0],
+      [180, 15],
+      [30, 15],
+      [30, 0]
     ],
-    description: 'Covers Lower South Asia, containing Maldives, Sri Lanka, parts of Malaysia, Cambodia, Philippines, Indonesia.',
+    description: 'Covers right part of Upper-mid Africa, and Lower South Asia.',
   },
   '51': {
     name: 'Slice 51',
-    bbox: '-180,15,-30,30',
+    bbox: '-180,15,-60,30',
     coordinates: [
-      [
-        [-180, 15],
-        [-30, 15],
-        [-30, 30],
-        [-180, 30],
-        [-180, 15],
-      ],
+      [-180, 15],
+      [-60, 15],
+      [-60, 30],
+      [-180, 30],
+      [-180, 15]
     ],
     description: 'Covers Lower North America, containing Mexico and Cuba.',
   },
   '52': {
     name: 'Slice 52',
-    bbox: '-30,15,60,30',
+    bbox: '-60,15,30,30',
     coordinates: [
-      [
-        [-30, 15],
-        [60, 15],
-        [60, 30],
-        [-30, 30],
-        [-30, 15],
-      ],
+      [-60, 15],
+      [30, 15],
+      [30, 30],
+      [-60, 30],
+      [-60, 15]
     ],
-    description: 'Covers Upper Africa, with parts of Egypt, Libya, Niger, Mauritania, Saudi Arabia, Oman, and Yemen.',
+    description: 'Covers left part of Upper Africa.',
   },
   '53': {
     name: 'Slice 53',
-    bbox: '60,15,180,30',
+    bbox: '30,15,180,30',
     coordinates: [
-      [
-        [60, 15],
-        [180, 15],
-        [180, 30],
-        [60, 30],
-        [60, 15],
-      ],
+      [30, 15],
+      [180, 15],
+      [180, 30],
+      [30, 30],
+      [30, 15]
     ],
-    description: 'Covers Upper South Asia, covering Nepal, Bangladesh, parts of Pakistan, China, and India.',
+    description: 'Covers right part of Upper Africa, lower part of Middle East, and Upper South Asia.',
   },
   '61': {
     name: 'Slice 61',
-    bbox: '-180,30,-30,45',
+    bbox: '-180,30,-60,45',
     coordinates: [
-      [
-        [-180, 30],
-        [-30, 30],
-        [-30, 45],
-        [-180, 45],
-        [-180, 30],
-      ],
+      [-180, 30],
+      [-60, 30],
+      [-60, 45],
+      [-180, 45],
+      [-180, 30]
     ],
     description: 'Covers Mid North America, covering most of the United States.',
   },
   '62': {
     name: 'Slice 62',
-    bbox: '-30,30,60,45',
+    bbox: '-60,30,30,45',
     coordinates: [
-      [
-        [-30, 30],
-        [60, 30],
-        [60, 45],
-        [-30, 45],
-        [-30, 30],
-      ],
+      [-60, 30],
+      [30, 30],
+      [30, 45],
+      [-60, 45],
+      [-60, 30]
     ],
-    description: 'Covers Lower western Europe, containing Spain, Italy, Turkey, parts of Italy, Iran, and Iraq.',
+    description: 'Covers lower western Europe.',
   },
   '63': {
     name: 'Slice 63',
-    bbox: '60,30,180,45',
+    bbox: '30,30,180,45',
     coordinates: [
-      [
-        [60, 30],
-        [180, 30],
-        [180, 45],
-        [60, 45],
-        [60, 30],
-      ],
+      [30, 30],
+      [180, 30],
+      [180, 45],
+      [30, 45],
+      [30, 30]
     ],
-    description: 'Covers Mid Asia, covering most of China, Japan, and parts of Afghanistan.',
+    description: 'Covers upper part of Middle East, and Middle Asia.',
   },
   '70': {
     name: 'Slice 70',
     bbox: '-180,45,180,60',
     coordinates: [
-      [
-        [-180, 45],
-        [180, 45],
-        [180, 60],
-        [-180, 60],
-        [-180, 45],
-      ],
+      [-180, 45],
+      [180, 45],
+      [180, 60],
+      [-180, 60],
+      [-180, 45]
     ],
     description: 'Covers Northern parts of United States, Europe, Asia, and the majority of Russia.',
   },
@@ -241,17 +207,14 @@ const slices = {
     name: 'Slice 80',
     bbox: '-180,60,180,90',
     coordinates: [
-      [
-        [-180, 60],
-        [180, 60],
-        [180, 90],
-        [-180, 90],
-        [-180, 60],
-      ],
+      [-180, 60],
+      [180, 60],
+      [180, 90],
+      [-180, 90],
+      [-180, 60]
     ],
     description: 'Covers the Northernmost regions of the world including Greenland, Arctic Ocean, and Northern Russia.',
   },
 };
 
 export default slices;
-
--- docs/geo-event-provider.md
+++ docs/geo-event-provider.md
@@ -9,21 +9,21 @@ The world is divided into eight horizontal slices based on latitude. Each slice 
 | Slice Name | Bounding Box     | Description |
 | :--------- | :--------------- | :---------- |
 | Slice 10    | "-180,-90,180,-30" | Covers the Southernmost regions of the world including the majority of Antarctica and parts of Southern South America. |
-| Slice 21    | "-180,-30,-30,-15" | Covers Mid-lower South America, containing upper Chile, Paraguay, lower Brazil, upper Argentina, lower Bolivia. |
-| Slice 22    | "-30,-30,60,-15" | Covers Lower Africa, including upper South Africa, Namibia, Botswana, Zimbabwe, parts of Madagascar. |
-| Slice 23    | "60,-30,180,-15" | Covers Upper part of Australia |
-| Slice 31    | "-180,-15,-30,0"   | Covers Mid-upper South America, covering upper Brazil, Peru, upper Bolivia. |
-| Slice 32    | "-30,-15,60,0"   | Covers Lower-mid Africa, with parts of Angola, Zambia, Malawi, Tanzania, Kenya, Democratic Republic of the Congo, Gabon, Republic of the Congo. |
-| Slice 33    | "60,-15,180,0"   | Covers Papua New Guinea and parts of Indonesia. |
-| Slice 41    | "-180,0,-30,15"    | Covers Upper South America, containing upper Colombia, Venezuela, Guyana, Suriname, Panama, Costa Rica, Nicaragua. |
-| Slice 42    | "-30,0,60,15"    | Covers Upper-mid Africa, covering Nigeria, Cameroon, parts of Sudan, Ethiopia, Somalia. |
-| Slice 43    | "60,0,180,15"    | Covers Lower South Asia, containing Maldives, Sri Lanka, parts of Malaysia, Cambodia, Philippines, Indonesia. |
-| Slice 51    | "-180,15,-30,30"   | Covers Lower North America, containing Mexico and Cuba. |
-| Slice 52    | "-30,15,60,30"   | Covers Upper Africa, with parts of Egypt, Libya, Niger, Mauritania, Saudi Arabia, Oman, and Yemen. |
-| Slice 53    | "60,15,180,30"   | Covers Upper South Asia, covering Nepal, Bangladesh, parts of Pakistan, China, and India. |
-| Slice 61    | "-180,30,-30,45"   | Covers Mid North America, covering most of the United States. |
-| Slice 62    | "-30,30,60,45"   | Covers Lower western Europe, containing Spain, Italy, Turkey, parts of Italy, Iran, and Iraq. |
-| Slice 63    | "60,30,180,45"   | Covers Mid Asia, covering most of China, Japan, and parts of Afghanistan. |
+| Slice 21    | "-180,-30,-60,-15" | Covers left part of mid-lower South America |
+| Slice 22    | "-60,-30,30,-15" | Covers right part of mid-lower South America, and left part of Lower Africa. |
+| Slice 23    | "30,-30,180,-15" | Covers right part of Lower Africa, and upper part of Australia |
+| Slice 31    | "-180,-15,-60,0"   | Covers left part of Mid-upper South America |
+| Slice 32    | "-60,-15,30,0"   | Covers right part of Mid-upper South America, and left part of lower-mid Africa. |
+| Slice 33    | "30,-15,180,0"   | Covers right part of lower-mid Africa, and Papua New Guinea and parts of Indonesia. |
+| Slice 41    | "-180,0,-60,15"    | Covers left part of Upper South America|
+| Slice 42    | "-60,0,30,15"    | Covers right part of Upper South America and left part of Upper-mid Africa |
+| Slice 43    | "30,0,180,15"    | Covers right part of Upper-mid Africa, and Lower South Asia. |
+| Slice 51    | "-180,15,-60,30"   | Covers Lower North America, containing Mexico and Cuba. |
+| Slice 52    | "-60,15,30,30"   | Covers left part of Upper Africa. |
+| Slice 53    | "30,15,180,30"   | Covers right part of Upper Africa, lower part of middle east, and Upper South Asia |
+| Slice 61    | "-180,30,-60,45"   | Covers Mid North America, covering most of the United States. |
+| Slice 62    | "-60,30,30,45"   | Covers lower western Europe. |
+| Slice 63    | "30,30,180,45"   | Covers upper part of middle east, and middle Asia. |
 | Slice 70    | "-180,45,180,60"   | Covers Northern parts of United States, Europe, Asia, and the majority of Russia. |
 | Slice 80    | "-180,60,180,90"   | Covers the Northernmost regions of the world including Greenland, Arctic Ocean, and Northern Russia. |
 
