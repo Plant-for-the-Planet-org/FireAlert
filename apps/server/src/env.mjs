a8cb1ad00192bfb51dcf69962f2351225e11faf8
Rupam Kairi <rupamkairi@hotmail.com>
2025-04-22 15:03:05 +0530

    added development support for Shadow Database in Next env.mjs

--- apps/server/src/env.mjs
+++ apps/server/src/env.mjs
@@ -8,6 +8,7 @@ const server = z.object({
   DATABASE_URL: z.string().url().optional(),
   DATABASE_PRISMA_URL: z.string().url(),
   DATABASE_URL_NON_POOLING: z.string().url(),
+  DATABASE_URL_NON_POOLING_SHADOW: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
@@ -64,7 +65,7 @@ const processEnv = {
   DATABASE_PRISMA_URL: process.env.DATABASE_PRISMA_URL,
   DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : process.env.DATABASE_PRISMA_URL,
   DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING ? process.env.DATABASE_URL_NON_POOLING : process.env.DATABASE_URL,
-  DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING ? process.env.DATABASE_URL_NON_POOLING : process.env.DATABASE_URL,
+  DATABASE_URL_NON_POOLING_SHADOW: process.env.DATABASE_URL_NON_POOLING_SHADOW ? process.env.DATABASE_URL_NON_POOLING_SHADOW : process.env.DATABASE_URL,
   // DATABASE_PRISMA_URL is set by VERCEL POSTGRES and had pooling built in.
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
