b80e1cb944c57f8fcfebb93923a86c2b91a7ac07
Rupam Kairi <rupamkairi@hotmail.com>
2025-04-29 16:45:31 +0530

    removed next-auth env validations.

--- apps/server/src/env.mjs
+++ apps/server/src/env.mjs
@@ -9,19 +9,6 @@ const server = z.object({
   DATABASE_PRISMA_URL: process.env.VERCEL ? z.string().url() : z.string().url().optional(),
   DATABASE_URL_NON_POOLING: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === "production"
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? (process.env.COOLIFY_FQDN ? `https://${process.env.COOLIFY_FQDN}` : str),
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string().min(1) : z.string().url(),
-  ),
-  // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-
   NEXT_PUBLIC_AUTH0_CLIENT_ID: z.string(),
   NEXT_PUBLIC_AUTH0_ISSUER: z.string(),
   NEXT_PUBLIC_AUTH0_DOMAIN: z.string(),
